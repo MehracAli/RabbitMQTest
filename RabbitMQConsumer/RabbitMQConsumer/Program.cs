4f03d80823b4d498114758c29a55ca02693c6e21
Mehraj Mammadov <mehrajvm@code.edu.az>
2024-02-06 18:29:52 +0400

    first commit

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://tqlppzrj:9rQtfXQ7aapr-TXu7Q1hkrn2N6EhUvK3@sparrow.rmq.cloudamqp.com/tqlppzrj");
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+	channel.QueueDeclare("mesajkuyrugu", true, false, false, null);
+	channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+
+	EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+	channel.BasicConsume("mesajkuyrugu", false, consumer);
+	consumer.Received += (sender, e) =>
+	{
+		//Thread.Sleep(int.Parse(args[0]));
+		//e.Body : Kuyruktaki mesajı verir.
+		Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+		channel.BasicAck(e.DeliveryTag, false);
+	};
+	Console.Read();
+}
---
+++ RabbitMQPublisher/RabbitMQPublisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://tqlppzrj:9rQtfXQ7aapr-TXu7Q1hkrn2N6EhUvK3@sparrow.rmq.cloudamqp.com/tqlppzrj");
+
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+	channel.QueueDeclare("mesajkuyrugu", true, false, false, null);
+	for (int i = 1; i <= 100; i++)
+	{
+		byte[] bytemessage = Encoding.UTF8.GetBytes($"sebepsiz boş yere ayrılacaksan -> {i}");
+		IBasicProperties properties = channel.CreateBasicProperties();
+		properties.Persistent = true;
+		channel.BasicPublish(exchange: "", routingKey: "mesajkuyrugu", body: bytemessage, basicProperties: properties);
+	}
+};
